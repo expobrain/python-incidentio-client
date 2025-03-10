d658b7230da872fb2da537078cf1d20fa5451ae9
expobrain <2404909+expobrain@users.noreply.github.com>
2025-03-10 01:11:19 +0000

    chore: update pre-commit hooks

--- incident_io_client/api/audit_logs/audit_logs_private_incident_membership_granted_v1.py
+++ incident_io_client/api/audit_logs/audit_logs_private_incident_membership_granted_v1.py
@@ -24,10 +24,8 @@ def _parse_response(
     *, client: Union[AuthenticatedClient, Client], response: httpx.Response
 ) -> Optional[AuditLogsPrivateIncidentMembershipGrantedV1ResponseBody]:
     if response.status_code == 200:
-        response_200 = (
-            AuditLogsPrivateIncidentMembershipGrantedV1ResponseBody.from_dict(
-                response.json()
-            )
+        response_200 = AuditLogsPrivateIncidentMembershipGrantedV1ResponseBody.from_dict(
+            response.json()
         )
 
         return response_200
